330e74ac8f07e66ac227da643fe9921f5fb91fd3
Pedro Jorge Lima de Lacerda <pjlacerda18@gmail.com>
2022-04-06 17:30:32 -0300

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    MatToolbarModule,
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
